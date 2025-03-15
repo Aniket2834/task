bd44572fa6edddaf0bfa4c86258f51321572bf54
aniket2834 <malianiket2525@gmail.com>
2025-03-15 19:03:31 +0530

    change

---
+++ text.js
@@ -0,0 +1,16 @@
+const arr = ["Hello", "I'm", "Aniket", "Mali", "And", "I'm", "a", "great", "dev"];
+
+function convert() {
+    const data = arr.sort()
+    
+}
+
+convert()
+
+/*
+[
+[a, And, Aniket],
+[Dev],
+...
+]
+*/
